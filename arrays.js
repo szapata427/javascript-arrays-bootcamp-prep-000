e13b353a4f210f8262e3f5ebfa839ee88505f222
Steven Zapata <szapata427@gmail.com>
2018-07-24 15:19:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   array
   array.slice(1)
-  return array
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
