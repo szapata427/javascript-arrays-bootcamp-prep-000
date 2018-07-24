9098b07d31c667e39a2f19f0950dffaf908e7ec1
Steven Zapata <szapata427@gmail.com>
2018-07-24 15:17:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ return [element, ...array]
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array
   array.unshift(element)
-  return array.unshift(element)
+  return array
 }
 
 
